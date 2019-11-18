a1b669dfcd2d7fcff8146248a375dd8ba5074516
ramror3 <ramesh.sabbanis3@gmail.com>
2019-11-18 15:55:34 +0530

    worked on globbing and dynamic routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,22 @@
 Rails.application.routes.draw do
+  
+  # get 'posts/index'
+
+	namespace :admin do
+	  get 'dashboard/main'
+	  get 'dashboard/user'
+	  get 'dashboard/blog'
+	end
+
   get 'about', to: 'pages#about' 
   get 'leadgen/advertising/gen/lead', to: 'pages#contact', as: 'lead'
 
   resources :blogs
+  resources :posts
+
+  get 'posts/*missing', to: 'posts#missing' #globing routes
+  get 'query/:else/:another_one', to: 'pages#something' #dynamic routes
+  get 'query/:else', to: 'pages#something'
+  
+  root to: 'pages#home'
 end
